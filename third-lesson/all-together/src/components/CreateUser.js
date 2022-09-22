43b3847db01f8389b6bf327d67b2127f4c5fc616
MohamedDiaaEldin <mdiaan404@gmail.com>
2022-09-22 16:03:55 +0200

    finish all together ex

--- third-lesson/all-together/src/components/CreateUser.js
+++ third-lesson/all-together/src/components/CreateUser.js
@@ -12,6 +12,7 @@ const CreateUser = ({users, setUsers})=>{
         userName:""
     })
 
+    // update user data 
     const handelChange = (event) => {
         const { name, value } = event.target;
         setUser({...user, [name]:value})
@@ -43,14 +44,30 @@ const CreateUser = ({users, setUsers})=>{
         return !user.firstName || !user.lastName || ! user.userName
     }
 
-    return <form className='create-user' onSubmit={addUser}>
-        <p className='error' >{error} </p>
-        <input  type="text" onChange={handelChange} name='firstName' placeholder='first name' />
-        <input type="text" onChange={handelChange} name='lastName' placeholder='last name' />
-        <input type="text" onChange={handelChange} name='userName' placeholder='user name' />
-        <button disabled={isDisable()} >Add</button>
-        
-    </form>
+    return (
+      <form className="create-user" onSubmit={addUser}>
+        <p className="error">{error} </p>
+        <input
+          type="text"
+          onChange={handelChange}
+          name="firstName"
+          placeholder="first name"
+        />
+        <input
+          type="text"
+          onChange={handelChange}
+          name="lastName"
+          placeholder="last name"
+        />
+        <input
+          type="text"
+          onChange={handelChange}
+          name="userName"
+          placeholder="user name"
+        />
+        <button disabled={isDisable()}>Add</button>
+      </form>
+    );
 }
 
 export default CreateUser
--- third-lesson/all-together/src/components/Users.js
+++ third-lesson/all-together/src/components/Users.js
@@ -2,23 +2,28 @@
 import  { useState } from 'react';
 
 const Users =({users}) =>{
-    const [showState, setGameState] = useState(true)
-    const [games, setGames] = useState(0)
+  const [showState, setGameState] = useState(true);
 
-    const hideHandler = ()=>{
-        setGameState(!showState)
-    }
-    return <div>
-        <h2>Users</h2>
-        <ol>
-            {users.map(user=>{        
-                     
-            return <li>{`${user.userName} have played ${games}  games`}</li>
-            })
-            }
-        </ol>
-        <button onClick={hideHandler}>{showState?'hide':'show'}</button>
+  const hideHandler = () => {
+    setGameState(!showState);
+  };
+  
+  return (
+    <div>
+      <h2>Users</h2>
+      <ol>
+        {users.map((user) => {              
+            const games =  showState ? user.games  : '*'
+          return (
+            <li
+              key={user.userName}
+            >{`${user.userName} have played ${games}  games`}</li>
+          );
+        })}
+      </ol>
+      <button onClick={hideHandler}>{ showState ? "hide" : "show"}</button>
     </div>
+  );
 }
 
 export default Users
